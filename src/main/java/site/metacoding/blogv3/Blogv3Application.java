e25d6e8a1ec88057f78428111694de77f3436db5
gih1214 <gih1214@hanmail.net>
2022-05-23 19:29:23 +0900

    ~~~ 엔티티 생성중...

--- src/main/java/site/metacoding/blogv3/Blogv3Application.java
+++ src/main/java/site/metacoding/blogv3/Blogv3Application.java
@@ -2,7 +2,9 @@ package site.metacoding.blogv3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // 날짜
 @SpringBootApplication
 public class Blogv3Application {
 
